2cf47b97285dc48375aa4b5f2eba8a0633477435
Yuvaan-Goel <78798914+Yuvaan-Goel@users.noreply.github.com>
2021-10-07 15:08:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function inn() {
+	window.location = "innovation_showing.html";
+}
+
+function ma() {
+	window.location = "mask.html";
+}
+
+function sn() {
+	window.location = "sanitizer.html";
+}
